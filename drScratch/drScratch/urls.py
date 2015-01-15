6c7c98dcc4855f7dd099f66fa9e170683b0303c4
Mari Luz <ml.aguadoj@gmail.com>
2015-01-15 15:49:46 +0100

    first commit

--- drScratch/drScratch/urls.py
+++ drScratch/drScratch/urls.py
@@ -4,22 +4,14 @@ from django.conf import settings
 
 urlpatterns = [
     # Examples:
-<<<<<<< HEAD
     # url(r'^$', 'drScratch.views.home', name='home'),
-=======
-    # url(r'^$', 'DrScratch.views.home', name='home'),
->>>>>>> c27e7480b09df45e2fcaef660b35f5b02f8dd680
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^resources/(?P<path>.*)$', 'django.views.static.serve', {'document_root' : settings.MEDIA_ROOT}),
     #url(r'^profile', 'DrScratchApp.views.profileSettings',),
     url(r'^progressBar', 'app.views.progressBar',),
-<<<<<<< HEAD
     url(r'^admin/upload_progress/$', 'app.views.upload_progress', name="admin-upload-progress"),
-=======
-    url(r'^admin/upload_progress/$', 'utils.views.upload_progress', name="admin-upload-progress"),
->>>>>>> c27e7480b09df45e2fcaef660b35f5b02f8dd680
     url(r'^login', 'app.views.loginUser',),
     url(r'^logout', 'app.views.logoutUser',),
     url(r'^uploadUnregistered', 'app.views.uploadUnregistered',),
