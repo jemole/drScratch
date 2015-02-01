b281127e5e0e32cf21eaab4a88ee111f18334a86
titua77 <cdavid.chushig@gmail.com>
2015-02-01 23:38:14 +0100

    First steps in pre

--- drScratch/settings.py
+++ drScratch/settings.py
@@ -27,7 +27,7 @@ STATIC_ROOT = '/resources/'
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 
+SECRET_KEY =
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
