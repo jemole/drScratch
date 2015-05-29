228f75250ab945a57275d9dbfa66d28e6067d3eb
misshu93 <evahugarres@gmail.com>
2015-05-29 14:04:04 +0200

    New dashboards with modals.

---
+++ static/app/scripts/magnific_modal.js
@@ -0,0 +1,12 @@
+$(function () {
+	$('.popup-modal').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#username',
+		modal: true
+	});
+	$(document).on('click', '.popup-modal-dismiss', function (e) {
+		e.preventDefault();
+		$.magnificPopup.close();
+	});
+});
