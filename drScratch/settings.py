49d182f11b671d636ab018067a004438c6d45ed1
meryblue <ml.aguadoj@gmail.com>
2015-07-22 12:07:14 +0200

    FECyT logo

--- drScratch/settings.py
+++ drScratch/settings.py
@@ -27,7 +27,7 @@ STATIC_ROOT = '/static/'
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '...'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -43,7 +43,7 @@ TEMPLATE_LOADERS = (
 
 TEMPLATE_DIRS = ('templates',)
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = [...]
 
 
 # Application definition
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'drScratch.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': '',
-        'NAME': '',
+        'NAME': ...,
 	'USER': '',
 	'PASSWORD':'',
 	'HOST': '',
@@ -105,13 +105,6 @@ LANGUAGES = (
     ('es', _('Spanish')),
     ('en', _('English')),
 )
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD=''
-
 
 
 LOCALE_PATHS = (
