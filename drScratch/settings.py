9505b58bc0ac3e724b906b9172e43bafc20053fc
meryblue <ml.aguadoj@gmail.com>
2016-03-04 11:50:01 +0100

    Settings

--- drScratch/settings.py
+++ drScratch/settings.py
@@ -27,7 +27,7 @@ STATIC_ROOT = '/static/'
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b&0vhl+ghtc+31i7xmq@$t)n^)*8p3a9!p8=pk2z9tb5om*(&l'
+SECRET_KEY = '...'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -41,10 +41,9 @@ TEMPLATE_LOADERS = (
 #'django.template.loaders.eggs.Loader',
 )
 
-
 TEMPLATE_DIRS = ('templates',)
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = [...]
 
 
 # Application definition
@@ -63,14 +62,12 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
-
-
 
 ROOT_URLCONF = 'drScratch.urls'
 
@@ -80,23 +77,23 @@ WSGI_APPLICATION = 'drScratch.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'drscratch',
-        'USER': 'root',
-        'PASSWORD': 'Holamundo314',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': '',
+        'NAME': ...,
+	'USER': '',
+	'PASSWORD':'',
+	'HOST': '',
+	'PORT': '',
     }
 }
+
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
 MEDIA_ROOT = 'static'
-MEDIA_URL = os.path.join(BASE_DIR, 'static/img/')
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
@@ -108,16 +105,18 @@ LANGUAGES = (
     ('es', _('Spanish')),
     ('en', _('English')),
     ('ca', _('Catalan')),
+    ('ca', _('Catalan')),
     ('gl', _('Galician')),
     ('pt', _('Portuguese')),
+    ('el', _('Greek')),
 )
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = False
-EMAIL_HOST = 'server.programamos.es'
-EMAIL_PORT = 2525
-EMAIL_HOST_USER = 'no-reply@drscratch.org'
-EMAIL_HOST_PASSWORD='N-r-drscratch'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
 LOCALE_PATHS = (
@@ -131,3 +130,4 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
