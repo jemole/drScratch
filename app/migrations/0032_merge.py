d7fc540c56e72195748b82952fd02a6f49128916
meryblue <ml.aguadoj@gmail.com>
2016-03-04 12:16:04 +0100

    Settings

---
+++ app/migrations/0032_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app', '0031_auto_20150921_1813'),
+        ('app', '0031_auto_20150914_1815'),
+    ]
+
+    operations = [
+    ]
--- drScratch/settings.py
+++ drScratch/settings.py
@@ -104,6 +104,11 @@ _ = lambda s: s
 LANGUAGES = (
     ('es', _('Spanish')),
     ('en', _('English')),
+    ('ca', _('Catalan')),
+    ('ca', _('Catalan')),
+    ('gl', _('Galician')),
+    ('pt', _('Portuguese')),
+    ('el', _('Greek')),
 )
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
